255c246b6a3f54f88e97422bf54d31060b35e0b7
Breno Oliveira <breno.oliveira@moip.com.br>
2016-06-24 14:50:28 -0300

    form login

--- app/src/main/java/br/com/atrasado/presentations/views/activities/SignUpActivity.java
+++ app/src/main/java/br/com/atrasado/presentations/views/activities/SignUpActivity.java
@@ -2,17 +2,27 @@ package br.com.atrasado.presentations.views.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import br.com.atrasado.atrasado.R;
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends Activity {
 
+
+    @Bind(R.id.signupName) EditText edtSignUpName;
+    @Bind(R.id.signupBirthdate) EditText edtBirthdate;
+    @Bind(R.id.signupCpf) EditText edtCpf;
+    @Bind(R.id.signupCreditCard) EditText edtCreditCard;
+    @Bind(R.id.signupExpiration) EditText edtExpiration;
+    @Bind(R.id.signupEmail) EditText edtEmail;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
     }
 }
