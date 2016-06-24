f382846bed4124da2a9ac795bb9577f2e43e9134
Breno Oliveira <breno.oliveira@moip.com.br>
2016-06-24 17:51:03 -0300

    add success screen

---
+++ app/src/main/java/br/com/atrasado/presentations/views/activities/SuccessActivity.java
@@ -0,0 +1,14 @@
+package br.com.atrasado.presentations.views.activities;
+
+import android.os.Bundle;
+
+import br.com.atrasado.R;
+
+public class SuccessActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
